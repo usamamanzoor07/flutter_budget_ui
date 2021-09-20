58c3ce34d9a597fd81ec3b27810669a177438a59
Muhammad Usama <usamamanzoor07@gmail.com>
2021-09-20 23:00:10 +0500

    Middle range Color Changed from purple to orange.

--- lib/helpers/color_helper.dart
+++ lib/helpers/color_helper.dart
@@ -4,7 +4,7 @@ Color getColor(BuildContext context, double percent){
   if(percent >= 0.50){
     return Theme.of(context).primaryColor;
   }else if(percent >= 0.25){
-    return Colors.purple;
+    return Colors.orange;
   }
   return Colors.red;
 }
